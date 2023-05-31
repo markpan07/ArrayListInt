956dcfbcf1506d114dccfb51ad3f4a74c357abda
МАРК <markpan07@mail.ru>
2023-05-31 16:47:14 +0300

    Algo2 is done

--- src/test/java/service/IntegerListImplTest.java
+++ src/test/java/service/IntegerListImplTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sky.pro.service.IntegerListImpl;
 
+import java.util.Random;
+
 class IntegerListImplTest {
     private IntegerListImpl out = new IntegerListImpl(4);
 
@@ -23,11 +25,29 @@ class IntegerListImplTest {
     }
 
     @Test
+    void bubbleSort() {
+        out.add(27);
+        out.add(4);
+        out.add(113);
+        out.add(1);
+        IntegerListImpl.bubbleSort(out);
+    }
+
+    @Test
+    void selectionSort() {
+        out.add(27);
+        out.add(4);
+        out.add(113);
+        out.add(1);
+        IntegerListImpl.selectionSort(out);
+    }
+
+    @Test
     void addInTheEnd() {
         out.add(21);
         out.add(3);
         out.add(7);
-        Integer[] expected = new Integer[]{5, 11, 21, 3, 7, null, null};
+        Integer[] expected = new Integer[]{5, 11, 21, 3, 7};
         Assertions.assertThat(expected).
                 usingRecursiveComparison().
                 isEqualTo(out.toArray());
@@ -36,7 +56,7 @@ class IntegerListImplTest {
     @Test
     void addByIndex() {
         out.add(1, 21);
-        Integer[] expected = new Integer[]{5, 21, 11, null};
+        Integer[] expected = new Integer[]{5, 21, 11};
         Assertions.assertThat(expected).
                 usingRecursiveComparison().
                 isEqualTo(out.toArray());
@@ -47,7 +67,7 @@ class IntegerListImplTest {
         out.add(21);
         out.add(3);
         out.add(2, 7);
-        Integer[] expected = new Integer[]{5, 11, 7, 21, 3, null, null};
+        Integer[] expected = new Integer[]{5, 11, 7, 21, 3};
         Assertions.assertThat(expected).
                 usingRecursiveComparison().
                 isEqualTo(out.toArray());
@@ -65,7 +85,7 @@ class IntegerListImplTest {
     @Test
     void set() {
         out.set(1, 27);
-        Integer[] expected = new Integer[]{5, 27, null, null};
+        Integer[] expected = new Integer[]{5, 27};
         Assertions.assertThat(expected).
                 usingRecursiveComparison().
                 isEqualTo(out.toArray());
@@ -73,7 +93,7 @@ class IntegerListImplTest {
 
     @Test
     void removeByIndex() {
-        Integer[] expected = new Integer[]{11, null, null, null};
+        Integer[] expected = new Integer[]{11};
         out.remove(0);
         Assertions.assertThat(expected).
                 isEqualTo(out.toArray());
@@ -97,7 +117,7 @@ class IntegerListImplTest {
     void removeByItem() {
         out.add(27);
         out.add(29);
-        Integer[] expected = new Integer[]{5, 11, 29, null, null, null, null};
+        Integer[] expected = new Integer[]{5, 11, 29};
         out.remove((Integer) 27);
         Assertions.assertThat(expected).
                 isEqualTo(out.toArray());
@@ -115,7 +135,12 @@ class IntegerListImplTest {
         out.add(27);
         out.add(33);
         out.add(15);
+        out.add(3);
+        out.add(66);
+        out.add(115);
         Assertions.assertThat(out.contains(27)).isTrue();
+        Assertions.assertThat(out.contains(66)).isTrue();
+        Assertions.assertThat(out.contains(15)).isTrue();
         Assertions.assertThat(out.contains(511)).isFalse();
 
     }
@@ -160,7 +185,66 @@ class IntegerListImplTest {
                 isEqualTo(out.get(0));
     }
 
-/*    @Test
+    @Test
+    void sortTest() {
+        Random r = new Random();
+        IntegerListImpl list = new IntegerListImpl(4);
+        IntegerListImpl list2 = new IntegerListImpl(4);
+        IntegerListImpl list3 = new IntegerListImpl(4);
+        for (int i = 0; i <= 1000; i++) {
+            list.add(r.nextInt(100));
+        }
+        for (int i = 0; i < list.size(); i++) {
+            list2.add(list.get(i));
+        }
+        for (int i = 0; i < list.size(); i++) {
+            list3.add(list.get(i));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(list2.get(i) + " ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(list3.get(i) + " ");
+        }
+        System.out.println(" ");
+
+
+        long time = System.currentTimeMillis();
+        IntegerListImpl.bubbleSort(list);
+        long timeForBubble = System.currentTimeMillis() - time;
+
+        long time2 = System.currentTimeMillis();
+        IntegerListImpl.selectionSort(list2);
+        long timeForSelection = System.currentTimeMillis() - time2;
+
+        long time3 = System.currentTimeMillis();
+        IntegerListImpl.insertionSort(list3);
+        long timeForInsertion = System.currentTimeMillis() - time3;
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println(" ");
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print(list2.get(i) + " ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < 50; i++) {
+            System.out.print(list3.get(i) + " ");
+        }
+        System.out.println(" ");
+
+        System.out.println(timeForBubble + " " + timeForSelection + " " + timeForInsertion);
+    }
+
+    @Test
     void testEquals() {
         IntegerListImpl list = new IntegerListImpl(4);
         IntegerListImpl list2 = new IntegerListImpl(6);
@@ -180,13 +264,14 @@ class IntegerListImplTest {
 
 
     }
+
     @Test
     void testEqualsNull() {
 
         Assertions.assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(out.equals(null));
+                .isThrownBy(() -> out.equals(null));
 
-    }*/
+    }
 
     @Test
     void size() {
@@ -206,13 +291,13 @@ class IntegerListImplTest {
     @Test
     void clear() {
         out.clear();
-        Integer[] expected = new Integer[]{null, null, null, null};
+        Integer[] expected = new Integer[]{};
         Assertions.assertThat(expected).
                 usingRecursiveComparison().
                 isEqualTo(out.toArray());
         out.add(15);
         out.add(27);
-        Integer[] expected2 = new Integer[]{15, 27, null, null};
+        Integer[] expected2 = new Integer[]{15, 27};
         Assertions.assertThat(expected2).
                 usingRecursiveComparison().
                 isEqualTo(out.toArray());
