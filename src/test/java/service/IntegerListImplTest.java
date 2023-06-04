216ba99e60cab429b48963895ce48577b1555489
МАРК <markpan07@mail.ru>
2023-06-04 13:50:14 +0300

    Algo2 is done

--- src/test/java/service/IntegerListImplTest.java
+++ src/test/java/service/IntegerListImplTest.java
@@ -25,6 +25,18 @@ class IntegerListImplTest {
     }
 
     @Test
+    void quickSort() {
+        out.add(27);
+        out.add(4);
+        out.add(113);
+        out.add(1);
+        Integer[] test = out.toArray();
+        int i = out.size();
+        IntegerListImpl.quickSort(out.toArray(), 0, out.size() - 1);
+        System.out.println(out.toArray());
+    }
+
+    @Test
     void bubbleSort() {
         out.add(27);
         out.add(4);
